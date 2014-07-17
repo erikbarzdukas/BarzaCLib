16f40dd3755ac989b541fe9a3af9f11b7c7d73a0
Bronky <erik.barzdukas@gmail.com>
2014-07-16 21:57:26 -0700

    Fixed typo in darray_algos.c

--- src/lcthw/darray_algos.c
+++ src/lcthw/darray_algos.c
@@ -12,7 +12,7 @@ int DArray_heapsort(DArray *array, DArray_compare cmp) {
 	return heapsort(array->contents, DArray_count(array), sizeof(void *), cmp);
 }
 
-int DArray_mregesort(DArray *array, DArray_compare cmp) {
+int DArray_mergesort(DArray *array, DArray_compare cmp) {
 
 	return mergesort(array->contents, DArray_count(array), sizeof(void *), cmp);
 }
